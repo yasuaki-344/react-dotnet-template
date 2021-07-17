0f4eaee386539181a77e386a7542070c969a5d3d
yasuaki-344 <ikausay.443@gmail.com>
2021-07-17 14:11:10 +0900

    Change to typescript file

---
+++ src/Web/ClientApp/src/App.tsx
@@ -0,0 +1,20 @@
+import React, { FC } from 'react';
+import { Route } from 'react-router';
+import { Layout } from './components/Layout';
+import { Home } from './components/Home';
+import { FetchData } from './components/FetchData';
+import { Counter } from './components/Counter';
+
+import './custom.css'
+
+export const App: FC = (): JSX.Element => {
+  return (
+    <Layout>
+      <Route exact path='/' component={Home} />
+      <Route path='/counter' component={Counter} />
+      <Route path='/fetch-data' component={FetchData} />
+    </Layout>
+  );
+}
+
+App.displayName = App.name;
