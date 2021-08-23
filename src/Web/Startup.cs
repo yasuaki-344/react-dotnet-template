e89d0e456eb595f6e61574047130e665b9ef531d
yasuaki-344 <ikausay.443@gmail.com>
2021-08-24 00:07:17 +0900

    Add DB context setting to Startup

--- src/Web/Startup.cs
+++ src/Web/Startup.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Web.Data;
 
 namespace Web
 {
@@ -20,6 +22,11 @@ namespace Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlite(
+                    Configuration.GetConnectionString("DefaultConnection")
+                )
+            );
 
             services.AddControllersWithViews();
 
