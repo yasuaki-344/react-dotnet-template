23c98b862b6d0e42b3095f24f6548f04b2384dc4
yasuaki-344 <miyoshi.yh@om.asahi-kasei.co.jp>
2022-02-14 14:59:32 +0900

    Move DTO to ApplicationCore project

--- src/ApplicationCore/Dto/WeatherForecast.cs
+++ src/ApplicationCore/Dto/WeatherForecast.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 
-namespace WebApi
+namespace ApplicationCore.Dto
 {
     public class WeatherForecast
     {
--- src/WebApi/Controllers/WeatherForecastController.cs
+++ src/WebApi/Controllers/WeatherForecastController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using ApplicationCore.Dto;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
