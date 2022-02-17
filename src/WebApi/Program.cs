7ebc02c1f3cfe8ab579f3bba821f34c48d4db60c
yasuaki-344 <ikausay.443@gmail.com>
2022-02-17 21:43:23 +0900

    Add swagger documentation setting

--- src/WebApi/Program.cs
+++ src/WebApi/Program.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using System;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 {
@@ -11,7 +13,26 @@ var builder = WebApplication.CreateBuilder(args);
     builder.Services.AddControllers();
     // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
     builder.Services.AddEndpointsApiExplorer();
-    builder.Services.AddSwaggerGen();
+    builder.Services.AddSwaggerGen(options =>
+    {
+        options.SwaggerDoc("v1", new OpenApiInfo
+        {
+            Version = "v1",
+            Title = "Sample API",
+            Description = "This is just sample",
+            TermsOfService = new Uri("https://example.com/tems"),
+            Contact = new OpenApiContact
+            {
+                Name = "Example Contact",
+                Url = new Uri("https://example.com/contact")
+            },
+            License = new OpenApiLicense
+            {
+                Name = "Example License",
+                Url = new Uri("https://example.com/license")
+            }
+        });
+    });
 }
 
 var app = builder.Build();
@@ -22,7 +43,7 @@ var app = builder.Build();
         app.UseSwagger();
         app.UseSwaggerUI(options =>
         {
-            options.SwaggerEndpoint("/swagger/v1/swagger.yml", "v1");
+            options.SwaggerEndpoint("/swagger/v1/swagger.yaml", "v1");
             options.RoutePrefix = string.Empty;
         });
     }
