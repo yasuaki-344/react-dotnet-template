57d65a3ef3008fa663ed9873d39ab8c3604f7050
yasuaki-344 <ikausay.443@gmail.com>
2022-02-20 16:36:33 +0900

    Add versioning setting

--- src/WebApi/Program.cs
+++ src/WebApi/Program.cs
@@ -26,6 +26,7 @@ var builder = WebApplication.CreateBuilder(args);
     builder.Services.AddControllers();
     // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
     builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddApiVersioning();
     builder.Services.AddSwaggerGen(options =>
     {
         options.SwaggerDoc("v1", new OpenApiInfo
