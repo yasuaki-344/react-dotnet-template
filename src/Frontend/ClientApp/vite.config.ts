620e4d0dfec7c727e70a30689fd67d01d639f6cf
yasuaki-344 <ikausay.443@gmail.com>
2023-03-20 22:22:46 +0900

    Moidfy vite.config.ts

--- src/Frontend/ClientApp/vite.config.ts
+++ src/Frontend/ClientApp/vite.config.ts
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: "build",
+    sourcemap: false,
+  },
+  server: {
+    proxy: {
+      "^/api/.*": "http://localhost:5000",
+    },
+  }
 })
