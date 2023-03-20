54b678c742f61e6722ab01a540f9eeec65288afa
yasuaki-344 <ikausay.443@gmail.com>
2023-03-20 18:47:14 +0900

    Create WeatherForecastService

---
+++ src/ApplicationCore/Services/WeatherForecastService.cs
@@ -0,0 +1,23 @@
+﻿using ApplicationCore.Dto;
+using ApplicationCore.Entities;
+using ApplicationCore.Interfaces;
+
+namespace ApplicationCore.Services;
+
+public class WeatherForecastService : IWeatherForecastService
+{
+    /// <summary>
+    /// Initializes a new instance of WeatherForecastService class.
+    /// </summary>
+    public WeatherForecastService()
+    {
+    }
+
+    /// <inheritdoc/>
+    public async Task<ICollection<WeatherForecastDto>> GetWeatherForecastsAsync()
+    {
+        await Task.CompletedTask;
+        throw new NotImplementedException();
+    }
+}
+
