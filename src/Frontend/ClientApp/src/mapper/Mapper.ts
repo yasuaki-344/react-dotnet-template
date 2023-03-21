3c361c9b20cc55651b568059085e6686a616bacf
yasuaki-344 <ikausay.443@gmail.com>
2023-03-21 11:28:55 +0900

    Create Mapper.ts

---
+++ src/Frontend/ClientApp/src/mapper/Mapper.ts
@@ -0,0 +1,32 @@
+import { createMap, createMapper } from "@automapper/core";
+import { pojos, PojosMetadataMap } from "@automapper/pojos";
+import { type WeatherForecast } from "../api-gateways";
+import { type WeatherForecastDto } from "../domain/WeatherForecastDto";
+
+// entity mete data
+PojosMetadataMap.create<WeatherForecast>("WeatherForecast", {
+  date: Date,
+  temperatureC: Number,
+  temperatureF: Number,
+  summary: String,
+});
+
+// DTO mete data
+PojosMetadataMap.create<WeatherForecastDto>("WeatherForecastDto", {
+  date: Date,
+  temperatureC: Number,
+  temperatureF: Number,
+  summary: String,
+});
+
+// create mapper
+const entityMapper = createMapper({
+  strategyInitializer: pojos(),
+});
+createMap<WeatherForecast, WeatherForecastDto>(
+  entityMapper,
+  "WeatherForecast",
+  "WeatherForecastDto"
+);
+
+export const mapper = entityMapper;
