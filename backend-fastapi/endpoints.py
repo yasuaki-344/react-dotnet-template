2ceee6ecb8e66ff4e53476628d5dc20d462dd1e5
yasuaki-344 <ikausay.443@gmail.com>
2024-06-02 22:32:41 +0900

    :label:Add response model

--- backend-fastapi/endpoints.py
+++ backend-fastapi/endpoints.py
@@ -1,12 +1,34 @@
 """Endpoints modules."""
-
+from typing import List
 from fastapi import APIRouter
+from pydantic import BaseModel
+from datetime import datetime
 from dependency_injector.wiring import inject
 
 router = APIRouter(tags=["WeatherForecast"])
 
 
-@router.get("/api/v1/weather-forecasts")
+class WeatherForecast(BaseModel):
+    date: datetime
+    temperature_c: int
+    temperature_f: int
+    summary: str
+
+
+@router.get("/api/v1/weather-forecasts", response_model=List[WeatherForecast])
 @inject
-def root():
-    return {"message": "Hello World"}
+def get_weather_forecast_list() -> List[WeatherForecast]:
+    return [
+        {
+            "date": datetime.now(),
+            "temperature_c": 10,
+            "temperature_f": 20,
+            "summary": "test",
+        },
+        {
+            "date": datetime.now(),
+            "temperature_c": 10,
+            "temperature_f": 20,
+            "summary": "test",
+        }
+    ]
